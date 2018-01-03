7234308ae046573575fc36a7bd7735a871b7a76d
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2018-01-03 17:42:55 +0900

    modify transform function

--- scripts/test_recognition_hr.py
+++ scripts/test_recognition_hr.py
@@ -16,7 +16,15 @@ from config import Config
 from espcn_model import Net as ESPCN
 from espcn_img import espcn_img
 
-def transform_img(img):
+from PIL import Image
+
+def transform_img(img_path, espcn_model = None):
+
+    if espcn_model is not None:
+        img = espcn_img(img_path, espcn_model)
+    else:
+        img = Image.open(img_path)
+        
     img = Config.preprocess(img)
 
     return img.unsqueeze(0)
@@ -30,10 +38,10 @@ if __name__=="__main__":
     espcn_model.load_state_dict(torch.load(Config.model_dir + Config.cnn_model))
     
     img_path_list = [Config.image_dir + "/roi_" + str(i) + ".jpg" for i in range(9)]
-    img_list = [transform_img(espcn_img(img_path, espcn_model)) for img_path in img_path_list]
+    img_list = [transform_img(img_path, espcn_model) for img_path in img_path_list]    
 
-    instance_img_path = Config.image_dir + "/fast_mask_roi_11.jpg"
-    instance_img_torch = Variable(transform_img(espcn_img(instance_img_path, espcn_model))).cuda()
+    instance_img_path = Config.image_dir + "/fast_mask_roi_10.jpg"
+    instance_img_torch = Variable(transform_img(instance_img_path, espcn_model)).cuda()
     instance_output = cls_model(instance_img_torch)
     instance_output = F.normalize(instance_output, p = 2, dim = 1)
 
